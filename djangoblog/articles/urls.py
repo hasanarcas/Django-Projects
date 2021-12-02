ff5e8b66005cc32a8ea272fda84a6d55fbd3864d
hasanarcas <hasanarcas@gmail.com>
2021-12-03 00:07:32 +0300

    Added styles.css

--- djangoblog/articles/urls.py
+++ djangoblog/articles/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^$', views.article_list),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
