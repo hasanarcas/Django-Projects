d0dbc70ca14e8d1448215424105054963957b735
hasanarcas <hasanarcas@gmail.com>
2021-12-03 17:51:11 +0300

    Added a few things

--- djangoblog/articles/urls.py
+++ djangoblog/articles/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from . import views
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+app_name = "articles"
 
 urlpatterns = [
-    url(r'^$', views.article_list),
+    url(r'^$', views.article_list, name="list"),
+    url(r'^(?P<slug>[\w-]+)/$', views.article_detail, name="detail"),
 ]
 
-urlpatterns += staticfiles_urlpatterns()
